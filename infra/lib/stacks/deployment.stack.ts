f4ea9b55a84cb2cc29395cbf32252421f578bdc6
Ashley Taylor <eashleta@amazon.co.uk>
2025-06-10 15:59:59 +0100

    Init db custom resource and local pg config

--- infra/lib/stacks/deployment.stack.ts
+++ infra/lib/stacks/deployment.stack.ts
@@ -1,10 +1,16 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { SecurityGroup, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Function as LambdaFunction } from 'aws-cdk-lib/aws-lambda';
+import { DatabaseInstance } from 'aws-cdk-lib/aws-rds';
 import { Construct } from 'constructs';
+import { InitRdsDatabase } from '../constructs/init-rds-database/init-rds-database.cr';
 import { SetApiLambdaEnvironment } from '../constructs/set-api-lambda-environment/set-api-lambda-environment.cr';
 
 export interface DeploymentStackProps extends StackProps {
   apiHandlerLambda: LambdaFunction;
+  rdsInstance: DatabaseInstance;
+  vpc: Vpc;
+  lambdaSecurityGroup: SecurityGroup;
   cloudFrontDistributionDomain: string;
   cognitoUserPoolId: string;
   cognitoUserPoolClientId: string;
@@ -27,6 +33,12 @@ export class DeploymentStack extends Stack {
         COGNITO_CLIENT_ID: props.cognitoUserPoolClientId,
         COGNITO_CLIENT_SECRET_NAME: props.cognitoUserPoolClientSecretId,
       },
+    });
+
+    new InitRdsDatabase(this, 'init-rds-database', {
+      rdsInstanceSecret: props.rdsInstance.secret!,
+      vpc: props.vpc,
+      lambdaSecurityGroup: props.lambdaSecurityGroup,
     });
   }
 }
